c4e13b1be4e869d5129ded484cd23ccbc8fccaf8
buckeyerichsen <buckey.erichsen@gmail.com>
2019-09-19 16:44:04 -0400

    almost

--- src/Job.java
+++ src/Job.java
@@ -1,15 +1,21 @@
 
 public class Job {
+    private String email;
+    private String name;
     private String company;
     private String jobTitle;
     private String startDate;
     private String endDate;
     private String description;
 
+
+
     public Job() {
     }
 
-    public Job(String company, String jobTitle, String startDate, String endDate, String description) {
+    public Job(String email, String name, String company, String jobTitle, String startDate, String endDate, String description) {
+        this.email = email;
+        this.name = name;
         this.company = company;
         this.jobTitle = jobTitle;
         this.startDate = startDate;
@@ -57,7 +63,25 @@ public class Job {
         this.description = description;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void displayText(){
+        System.out.println(getName());
+        System.out.println(getEmail());
         System.out.println(getCompany());
         System.out.println(getDescription());
         System.out.println(getEndDate());
--- src/JobApp.java
+++ src/JobApp.java
@@ -15,6 +15,7 @@ public class JobApp {
         String description;
         ArrayList<Job> jobs = new ArrayList<>();
         int counter = 1;
+        Job job2 = new Job();
 
 
         System.out.println("please enter in the information below. victor es mi mejores amigo");
@@ -22,16 +23,20 @@ public class JobApp {
 
             System.out.println("please enter your name");
             name = sc.nextLine();
+            job2.setName(name);
+
 
 
             System.out.println("please enter your email address");
             email = sc.nextLine();
+            job2.setEmail(email);
+
+            jobs.add(job2);
 
 
             System.out.println("please enter former jobs.");
             while(true){
                 Job job1 = new Job();
-                System.out.println(" ");
                 System.out.println("what was the name of your company? " + counter);
                 company = sc.nextLine();
                 job1.setCompany(company);
