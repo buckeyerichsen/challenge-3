8d6da8e504c50b34078796f6af919a3b9145f705
buckeyerichsen <buckey.erichsen@gmail.com>
2019-09-19 16:20:30 -0400

    done

---
+++ src/Job.java
@@ -0,0 +1,67 @@
+
+public class Job {
+    private String company;
+    private String jobTitle;
+    private String startDate;
+    private String endDate;
+    private String description;
+
+    public Job() {
+    }
+
+    public Job(String company, String jobTitle, String startDate, String endDate, String description) {
+        this.company = company;
+        this.jobTitle = jobTitle;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void displayText(){
+        System.out.println(getCompany());
+        System.out.println(getDescription());
+        System.out.println(getEndDate());
+        System.out.println(getJobTitle());
+        System.out.println(getStartDate());
+    }
+}
---
+++ src/JobApp.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class JobApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String input;
+        String name;
+        String email;
+        String company;
+        String jobTitle;
+        String startDate;
+        String endDate;
+        String description;
+        ArrayList<Job> jobs = new ArrayList<>();
+        int counter = 1;
+
+
+        System.out.println("please enter in the information below. victor es mi mejores amigo");
+
+
+            System.out.println("please enter your name");
+            name = sc.nextLine();
+
+
+            System.out.println("please enter your email address");
+            email = sc.nextLine();
+
+
+            System.out.println("please enter former jobs.  if no more jobs to enter type stop");
+            while(true){
+                Job job1 = new Job();
+                System.out.println(" ");
+                System.out.println("what was the name of your company? " + counter);
+                company = sc.nextLine();
+                job1.setCompany(company);
+
+                System.out.println("what was your job title? " + counter);
+                jobTitle = sc.nextLine();
+                job1.setJobTitle(jobTitle);
+
+                System.out.println("what was the start date of your job? " + counter);
+                startDate = sc.nextLine();
+                job1.setStartDate(startDate);
+
+                System.out.println("what was the end date of your job? " + counter);
+                endDate = sc.nextLine();
+                job1.setEndDate(endDate);
+
+                System.out.println("how would you describe your job?");
+                description = sc.nextLine();
+                job1.setDescription(description);
+
+
+                jobs.add(job1);
+                System.out.println("enter stop if you are ready to stop, otherwise press any key.");
+                input = sc.nextLine();
+                if(input.equalsIgnoreCase("stop")){
+                    break;
+                }
+                counter = counter + 1;
+
+        }
+            counter =1;
+            for (Job job : jobs){
+                System.out.println("Job " + counter);
+                job.displayText();
+
+                counter = counter + 1;
+            }
+    }
+}
